e094d2beb515ffb0757103f0f9026295dbcf8ded
Sanjeev-blip <66805414+Sanjeev-blip@users.noreply.github.com>
2021-10-05 21:29:25 +1300

    show.blade.php done

--- resources/views/competitions/show.blade.php
+++ resources/views/competitions/show.blade.php
@@ -20,34 +20,74 @@
           height: 450px;
           position: relative;
         }
-        .content::before{
-          width: 80%;
-          height: 45%;
-          margin: auto;
-          position: absolute;
-          bottom: -30%;
-          left: 50%;
-          transform: translate(-50%, -50%);
-          content: '';
-          background: rgba(210, 210, 210, 0.9);
-          z-index: 1;
-          border-radius: 5px;
-        }
         .box{
-          width: 80%;
+          width: 70%;
+          padding: 10px 20px;
           margin: auto auto 10px auto;
           position: absolute;
-          bottom: -8%;
-          left: 60%;
+          left: 50%;
           transform: translate(-50%, -50%);
           color: black;
           z-index: 2;
+          background: rgba(210, 210, 210, 0.9);
+          border-radius: 5px;
+          text-align: center;
         }
         h4{
-          font-family: sans-serif;
+          font-family: 'Monda';
+        }
+        
+        @media screen and (max-width:500px){
+          .content::before {
+            height: 45%;
+            bottom: 5%;
+          }
+          .content img{
+            height: 600px;
+          }
+
+          .box{
+            padding-bottom: 50px; ;
+          }
+          .pl-2{
+            font-size: 12px;
+          }
+          .pl-2 p{
+            font-size: 12px;
+          }
+
+          .judge{
+            display: flex;
+            flex-direction:column;
+          }
+          .button{
+            margin: auto;
+          }
+          .button a{
+            font-size: 14px;
+          }
+          .footer{
+            margin-top: 100px !important;
+          }
         }
 
+        @media screen and (min-width:700px) and (max-width:1024px) {
+          .content::before{
+            bottom: -50%;
+            height: 25%;
+          }
 
+          .box{
+            bottom: 42%;
+            left: 55%;  
+          }
+
+          .content img{
+            height: 500px;
+          }
+
+        }       
+        
       </style>
     </head>
     <body>
@@ -55,19 +95,23 @@
         <hr>
         <div class="content">
           <img src="{{$competition->image_path}}" alt="">
-          <div class="box row place-content-cente row-cols-sm-2 row-cols-md-2 g-2">
-            <div class="col mt-10 pl-2">
+          <div class="box row row-cols-sm-2 row-cols-md-2 g-2">
+            <div class="col-12  mt-10 pl-2">
               <div class="pl-2">
                 <h4>Photo Contest ID:{{$competition->id}}</h4>
                 <h4 class="text-capitalize">{{$competition->title}}</h4>
                 <p>{{$competition->description}}</p>
               </div>
             </div>
-            <div class="col px-2 mt-10 ">
-              <div class="pl-2">
-                <h4 class="text-capitalize">{{$competition->time_remaining}}</h4>
-                <h6>Judge <a href="#">Manoj Rai</a></h6>
-                <a href="/competitions/{{$competition->id}}/entries/create" class="btn btn-success mt-2">Upload Photo</a>
+            <div class="col-12 px-2 mt-10 ">
+              <div class="judge pl-2">
+                <div class="h">
+                  <h4 class="text-capitalize">{{$competition->time_remaining}}</h4>
+                  <h6>Judge <a href="#">Manoj Rai</a></h6>
+                </div>
+                <div class="button" >
+                 <a href="/competitions/{{$competition->id}}/entries/create" class="btn btn-success">Upload Photo</a>
+                </div>
               </div>
             </div>
           </div>
