9f6c6f20ae41faf237f2702ef99899a683757c86
Manoj.Rai <2020000014@student.sit.ac.nz>
2021-10-05 16:46:45 +1300

    App title added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,8 @@
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-   <title>{{ config('app.name', 'Photocomp') }}</title>
+   <!-- // <title>{{ config('app.name', 'Photocomp') }}</title> -->
+   <title>Photocomp</title>
    <link rel="stylesheet" href="/app.css">
 
    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
