9a167eccb4c235b385989dbe15023749f9386790
Manoj.Rai <2020000014@student.sit.ac.nz>
2021-10-06 14:13:01 +1300

    seeder chagne

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
       \App\Models\User::factory(1)->create(['email' => 'a@a.com', 'username' => 'ma', 'admin' => 1]);
       \App\Models\User::factory(1)->create();
-      \App\Models\Competition::factory()->create(['end_time'=>now()]);
+      // \App\Models\Competition::factory()->create(['end_time'=>now()]);
       \App\Models\Competition::factory()->create();
 
     }
