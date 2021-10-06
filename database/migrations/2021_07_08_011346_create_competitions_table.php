53c11f043b95608a88b971928d0ed9fd1861270d
Manoj.Rai <2020000014@student.sit.ac.nz>
2021-10-06 13:32:02 +1300

    made to now(end_time)

--- database/migrations/2021_07_08_011346_create_competitions_table.php
+++ database/migrations/2021_07_08_011346_create_competitions_table.php
@@ -19,8 +19,8 @@ class CreateCompetitionsTable extends Migration
             $table->text('title');
             $table->text('description');
             $table->text('time_remaining');
-            $table->dateTime('start_time')->nullable();
-            $table->text('end_time')->nullable();
+            $table->dateTime('start_time')->now();
+            $table->text('end_time')->now()->addhours(1);
             $table->timestamps();
         });
     }
